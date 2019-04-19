bb5d1239441e22cd840be6ee3a53ac3466fc3de7
Bozhidar Dimitrov <Bozhidar.Dimitrov@endava.com>
2019-04-19 14:06:21 +0300

    added deletion of directory recursively in depth

--- core/navigator.go
+++ core/navigator.go
@@ -1,15 +1,14 @@
-package navigator
+package core
 
 import (
 	"fmt"
 
-	"github.com/bojodimitrov/byfiri/core"
 	"github.com/bojodimitrov/byfiri/structures"
 )
 
 // EnterDirectory returns DirectoryIterator of the desired directory within current directory
 func EnterDirectory(storage []byte, current *structures.DirectoryIterator, directory string) (*structures.DirectoryIterator, error) {
-	currentDirectoryContent := core.ReadDirectory(storage, current.DirectoryInode)
+	currentDirectoryContent := ReadDirectory(storage, current.DirectoryInode)
 	inode := 0
 	for _, dirEntry := range currentDirectoryContent {
 		if dirEntry.FileName == directory {
@@ -19,12 +18,12 @@ func EnterDirectory(storage []byte, current *structures.DirectoryIterator, direc
 	if inode == 0 {
 		return nil, fmt.Errorf("enter directory: name not found")
 	}
-	fsdata := core.ReadMetadata(storage)
-	inodeInfo := core.ReadInode(storage, fsdata, inode)
+	fsdata := ReadMetadata(storage)
+	inodeInfo := ReadInode(storage, fsdata, inode)
 	if inodeInfo.Mode != 0 {
 		return nil, fmt.Errorf("enter directory: file is not directory")
 	}
-	wantedDirectoryContent := core.ReadDirectory(storage, inode)
+	wantedDirectoryContent := ReadDirectory(storage, inode)
 	wantedDirectoryIt := structures.DirectoryIterator{DirectoryInode: inode, DirectoryContent: wantedDirectoryContent}
 	return &wantedDirectoryIt, nil
 }

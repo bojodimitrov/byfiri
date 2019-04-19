5bdfc2e89ec92d39567f0049f0df50d18363c8ef
Bozhidar Dimitrov <Bozhidar.Dimitrov@endava.com>
2019-04-19 11:35:19 +0300

    Add integration tests to cover more cases and fix bug found in file deletion.

--- core/destructors.go
+++ core/destructors.go
@@ -16,8 +16,27 @@ func deleteBlocks(storage []byte, fsdata *structures.Metadata, inodeInfo *struct
 	}
 }
 
+func removeFileFromDirectory(storage []byte, current *structures.DirectoryIterator, fileInode int) {
+	content := ReadDirectory(storage, current.DirectoryInode)
+
+	index := 0
+	for i, entry := range content {
+		if entry.Inode == uint32(fileInode) {
+			index = i
+		}
+	}
+
+	content[index] = content[len(content)-1]
+	content = content[:len(content)-1]
+
+	UpdateDirectory(storage, current.DirectoryInode, content)
+
+	current.DirectoryContent[index] = current.DirectoryContent[len(current.DirectoryContent)-1]
+	current.DirectoryContent = current.DirectoryContent[:len(current.DirectoryContent)-1]
+}
+
 //DeleteFile deletes file
-func DeleteFile(storage []byte, inode int) {
+func DeleteFile(storage []byte, currentDirectory *structures.DirectoryIterator, inode int) {
 	if inode == 0 {
 		fmt.Println("delete file: inode cannot be 0")
 		return
@@ -26,10 +45,12 @@ func DeleteFile(storage []byte, inode int) {
 		fmt.Println("delete file: cannot delete root")
 		return
 	}
+
 	fsdata := ReadMetadata(storage)
 	inodeInfo := ReadInode(storage, fsdata, inode)
 	clearFile(storage, inodeInfo.BlocksLocations, fsdata)
 	clearInode(storage, fsdata, inode)
+	removeFileFromDirectory(storage, currentDirectory, inode)
 	deleteBlocks(storage, fsdata, inodeInfo)
 	deleteInode(storage, fsdata, inode)
 }
--- tests/integration/core/fileupdate__test.go
+++ tests/integration/core/fileupdate__test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/bojodimitrov/byfiri/core"
+	"github.com/bojodimitrov/byfiri/structures"
 )
 
 func TestFileUpdate(t *testing.T) {
@@ -38,5 +39,36 @@ func TestFileEnlarge(t *testing.T) {
 	if resultFile1 != enlargeContent || resultFile2 != content {
 		t.Errorf("file1 content len: got %d, want %d", len(resultFile1), len(enlargeContent))
 		t.Errorf("file2: got %q, want %q", resultFile2, content)
+	}
+}
+
+func TestDirectoryUpdate(t *testing.T) {
+	storage, root := setupFileSystem()
+
+	updatedContent := []structures.DirectoryEntry{
+		structures.DirectoryEntry{FileName: ".", Inode: uint32(root.DirectoryInode)},
+		structures.DirectoryEntry{FileName: "..", Inode: 0},
+		structures.DirectoryEntry{FileName: "test file", Inode: 2},
+	}
+
+	expectedPure := []structures.DirectoryEntry{
+		structures.DirectoryEntry{FileName: ".", Inode: uint32(root.DirectoryInode)},
+		structures.DirectoryEntry{FileName: "..", Inode: 0},
+	}
+
+	resultPure := core.ReadDirectory(storage, root.DirectoryInode)
+
+	core.UpdateDirectory(storage, root.DirectoryInode, updatedContent)
+	resultUpdated := core.ReadDirectory(storage, root.DirectoryInode)
+
+	for i, entry := range resultPure {
+		if entry.FileName != expectedPure[i].FileName || entry.Inode != expectedPure[i].Inode {
+			t.Errorf("got %q, want %q", resultPure, expectedPure)
+		}
+	}
+	for i, entry := range resultUpdated {
+		if entry.FileName != updatedContent[i].FileName || entry.Inode != updatedContent[i].Inode {
+			t.Errorf("got %q, want %q", resultPure, resultUpdated)
+		}
 	}
 }
